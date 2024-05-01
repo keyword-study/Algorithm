da0263309a62c92ac7cd98548d18dc906e99286b
Sanha <98083809+kimsanhaa@users.noreply.github.com>
2024-05-01 21:11:36 +0900

    init

---
+++ keyword/src/test/java/key/word/study/keyword/KeywordApplicationTests.java
@@ -0,0 +1,13 @@
+package key.word.study.keyword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeywordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
