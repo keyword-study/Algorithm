496efec7f6ad22497abadb475475c433e8a8b10c
Sanha <98083809+kimsanhaa@users.noreply.github.com>
2024-05-30 22:27:43 +0900

    init

---
+++ keyword/src/test/java/key/word/study/keyword/immutable.java
@@ -0,0 +1,11 @@
+package key.word.study.keyword;
+
+public class immutable {
+    public static void main(String[] args) {
+        Integer test= 1;
+        test =2;
+        test =3;
+        test =5;
+
+    }
+}
